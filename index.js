f748ab83de75bd062d9fe508c284d2dab5410b55
kirby81 <etienne.ellie@gmail.com>
2021-07-06 10:17:53 +0200

    Add process params

--- index.js
+++ index.js
@@ -16,10 +16,62 @@ function process_cookies(cookies) {
         case 'string':
             return cookies;
         case 'object':
-            return cookies;
+            var cookies_array = [];
+            for (key in cookies) {
+                cookies_array.push('${key}=${cookies[key]}');
+            }
+            return cookies_array.join(';');
         default:
             Error('Cookies must be a string or object');
     }
+}
+
+function process_extract_rules(extract_rules) {
+    if (typeof extract_rules === 'object') {
+        return JSON.stringify(extract_rules);
+    } else {
+        Error('Extract rules must be an object');
+    }
+}
+
+function is_empty(value) {
+    switch (typeof value) {
+        case 'string':
+            return value === '';
+        case 'object':
+            return value && Object.keys(value).length === 0 && value.constructor === Object;
+        default:
+            return false;
+    }
+}
+
+function process_params(params) {
+    var clean_params = {};
+
+    for (key in params) {
+        if (is_empty(params[key])) {
+            continue;
+        }
+
+        switch (key) {
+            case 'url':
+                clean_params[key] = process_url(params[key]);
+                break;
+            case 'js_snippet':
+                clean_params[key] = process_js_snippet(params[key]);
+                break;
+            case 'cookies':
+                clean_params[key] = process_cookies(params[key]);
+                break;
+            case 'extract_rules':
+                clean_params[key] = process_extract_rules(params[key]);
+                break;
+            default:
+                clean_params[key] = params[key];
+        }
+    }
+
+    return clean_params;
 }
 
 function process_headers(headers, prefix = 'Spb-') {
@@ -57,6 +109,7 @@ export class ScrapingBeeClient {
         }
 
         // Process params
+        params = process_params(params);
 
         return axios({
             url: '',
