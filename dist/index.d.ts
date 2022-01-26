4a0bca9a5aa69e94f7f886302435b710d02a74ec
Pierre de Wulf <pierredewulf31@gmail.com>
2022-01-26 19:30:34 +0100

    Fix lock file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,6 +3,6 @@ export declare class ScrapingBeeClient {
     readonly api_key: string;
     constructor(api_key: string);
     private request;
-    get(config?: Record<string, any>): AxiosPromise<never>;
-    post(config?: Record<string, any>): AxiosPromise<never>;
+    get(config?: Record<string, any>): AxiosPromise<any>;
+    post(config?: Record<string, any>): AxiosPromise<any>;
 }
